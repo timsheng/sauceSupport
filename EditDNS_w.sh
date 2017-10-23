bf9c2947e392cce32ba8361cabd1741c54917cf4
tim.sheng <tim.sheng@overseasstudentliving.com>
2017-10-23 13:52:07 +0800

    add host file for windows

---
+++ EditDNS_w.sh
@@ -0,0 +1,4 @@
+@echo off
+echo 52.74.60.194 hurricane-www.dandythrust.com > %temp%\temphosts.txt
+type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
+copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
